cc1ce8cfd711e5121fa400d95183277cf60d5638
Maxim Kuznetsov <melixmilan@gmail.com>
2025-04-28 17:03:20 +0500

    move from `Eclipse IDE` to `IntelliJ IDEA`

--- Labs/src/lr3/CollectionsAndMapsTask.java
+++ Labs/src/lr3/CollectionsAndMapsTask.java
@@ -1,9 +1,5 @@
 package lr3;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.ArrayDeque;
-
 public class CollectionsAndMapsTask {
 	public static void main(String[] args) {
 	}
